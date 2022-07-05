fa0b72b0db629bf5fe5d5b68d27365599ff3ab0a
bradn4 <bradn4@smtp.bnorthern.info>
2022-07-05 16:27:59 -0500

    init

--- src/main/java/net/northern/ParsePDF.java
+++ src/main/java/net/northern/ParsePDF.java
@@ -3,7 +3,7 @@ package net.northern;
 public class ParsePDF {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello ");
     }
 
 }
