141fc8d11241f38cf04045b2ce24a9746d329156
Bnorthern42 <bradn4@gmail.com>
2022-07-12 19:49:33 -0500

    code cleanup - test-js

--- test/test.js
+++ test/test.js
@@ -4,18 +4,18 @@ const assert = require('assert');
 
 Lib.SearchPDF("dummy.pdf", "Dummy", "--bool")
 	.then(out => {
-	console.log("assert Dummy is pulled");
-	assert.equal(out, "True")
+		console.log("assert Dummy is pulled");
+		assert.equal(out, "True")
 }).catch(err =>{
-	  console.log(err);
+	console.log(err);
 });
 
 Lib.SearchPDF("large.pdf", "lo*re*", "--bool")
 	.then(out => {
-	console.log("Assert with wild cards");
-	assert.equal(out, "True")
+		console.log("Assert with wild cards");
+		assert.equal(out, "True")
 }).catch(err =>{
-	  console.log(err);
+	console.log(err);
 });
 
 
